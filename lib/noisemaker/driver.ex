b4f6a4c1b15ba866f9d9ab71c8d10f71b55db3fd
James <james@rectangle.pizza>
2022-08-01 19:44:26 -0500

    fix pattern matching bug

--- lib/noisemaker/driver.ex
+++ lib/noisemaker/driver.ex
@@ -35,7 +35,7 @@ defmodule Noisemaker.Driver do
     opts = Keyword.merge(@default_opts, opts)
     mapping = create_mapping(opts)
     pins = for {n, _cb} <- mapping do
-      pin = GPIO.open(n, :input, pull_mode: :pullup)
+      {:ok, pin} = GPIO.open(n, :input, pull_mode: :pullup)
       :ok = GPIO.set_interrupts(pin, :rising)
       pin
     end
