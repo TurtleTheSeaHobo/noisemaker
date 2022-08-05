5d4ae0cc929ecd061d4a13c26b5166044675b36e
James <james@rectangle.pizza>
2022-08-05 13:08:26 -0500

    implemented rudimentary software debounce

--- lib/noisemaker/driver.ex
+++ lib/noisemaker/driver.ex
@@ -1,18 +1,17 @@
 defmodule Noisemaker.Driver do
   use GenServer
   alias Circuits.GPIO
-  alias Noisemaker.Controller
+  #alias Noisemaker.Controller
 
   @default_opts [
-    select_pins: [4, 5, 6, 7, 8, 9, 10, 11],
-    bank_pins: [22, 23],
-    volume_pin: 12,
-    mode_pin: 13,
-    led_even_pin: 23,
-    led_odd_pin: 24, 
+    #select_pins: [4, 5, 6, 7, 8, 9, 10, 11, 22, 23],
+    #volume_pin: 12,
+    #mode_pin: 13,
+    button_pins: [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 22, 23],
+    led_pins: {24, 25}, 
   ]
 
-  defstruct [:mapping, :leds]
+  defstruct [:leds, :timers]
 
   def child_spec(opts) do
     %{
@@ -28,35 +27,49 @@ defmodule Noisemaker.Driver do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def led(pid \\ __MODULE__, even, odd) do
-    GenServer.cast(pid, {:led, even, odd}) 
+  def led(even, odd) do
+    GenServer.cast(__MODULE__, {:led, even, odd}) 
   end
   
   @impl true
   def init(opts) do
     opts = Keyword.merge(@default_opts, opts)
-    mapping = create_mapping(opts)
     
-    for {n, _cb} <- mapping do
-      {:ok, pin} = GPIO.open(n, :input)
-      :ok = GPIO.set_interrupts(pin, :falling)
+    button_pins = for n <- opts[:button_pins] do
+      {:ok, pin} = GPIO.open(n, :input, pull_mode: :pullup)
+      :ok = GPIO.set_interrupts(pin, :both)
       pin
+    end
+
+    timers = for pin <- button_pins, into: %{} do
+      {pin, nil}
     end
 
     {:ok, led_even_pin} = GPIO.open(opts[:led_even_pin], :output)
     {:ok, led_odd_pin} = GPIO.open(opts[:led_odd_pin], :output)
 
     state = %__MODULE__{
-      mapping: mapping,
       leds: {led_even_pin, led_odd_pin},
+      timers: timers,
     }
 
     {:ok, state}
   end
 
   @impl true
-  def handle_info({:circuits_gpio, pin, _, 0}, state) do
-    state.mapping[pin].()
+  def handle_info({:circuits_gpio, pin, _, value}, state) do
+    if state.timers[pin] do
+      Process.cancel_timer(state.timers[pin])
+    end
+
+    msg = {:debounced, pin, value}
+    timer = Process.send_after(self(), msg, 20)
+
+    {:noreply, %__MODULE__{state | timers: %{state.timers | pin => timer}}}
+  end
+
+  def handle_info({:debounced, pin, value}, state) do
+    IO.puts "debounced pin state: #{pin} = #{value}"
     {:noreply, state}
   end
 
@@ -69,28 +82,28 @@ defmodule Noisemaker.Driver do
     {:noreply, state}
   end
 
-  defp create_mapping(opts) do
-    sel_pins = opts[:select_pins]
-    sel_map = sel_pins
-              |> Enum.zip(0..(length(sel_pins) - 1))
-              |> Enum.map(fn {p, i} -> 
-                   {p, fn -> Controller.select(i) end} 
-                 end)
-              |> Enum.into(%{})
+  #defp create_mapping(opts) do
+  #  sel_pins = opts[:select_pins]
+  #  sel_map = sel_pins
+  #            |> Enum.zip(0..(length(sel_pins) - 1))
+  #            |> Enum.map(fn {p, i} -> 
+  #                 {p, fn -> Controller.select(i) end} 
+  #               end)
+  #            |> Enum.into(%{})
 
-    bank_pins = opts[:bank_pins]
-    bank_map = bank_pins
-               |> Enum.zip(0..(length(bank_pins) - 1))
-               |> Enum.map(fn {p, i} ->
-                    {p, fn -> Controller.bank(i) end}
-                  end)
-               |> Enum.into(%{})
+  #  bank_pins = opts[:bank_pins]
+  #  bank_map = bank_pins
+  #             |> Enum.zip(0..(length(bank_pins) - 1))
+  #             |> Enum.map(fn {p, i} ->
+  #                  {p, fn -> Controller.bank(i) end}
+  #                end)
+  #             |> Enum.into(%{})
 
-    sel_map
-    |> Map.merge(bank_map)
-    |> Map.merge(%{
-         opts[:volume_pin] => Controller.volume/0,
-         opts[:mode_pin] => Controller.mode/0,
-       })
-  end
+  #  sel_map
+  #  |> Map.merge(bank_map)
+  #  |> Map.merge(%{
+  #       opts[:volume_pin] => Controller.volume/0,
+  #       opts[:mode_pin] => Controller.mode/0,
+  #     })
+  #end
 end
